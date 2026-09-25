78a09022975994d718c4ed3016ad626803e5dd16
Santosh Raj Tuladhar <santoshtuladhar26@gmail.com>
2026-09-25 14:45:45 +0000

    Add lab3 assignment

---
+++ lab3/task5_control.c
@@ -0,0 +1,21 @@
+ #include <stdio.h>
+ #include <unistd.h>
+
+ int main() {
+ int choice;
+
+ // Print PID at start
+ printf("Current PID: %d\n", getpid());
+
+ printf("Do you want to continue? (1 for Yes, 0 for No): ");
+ scanf("%d", &choice);
+
+ if (choice == 1) {
+ printf("Continuing...\n");
+ sleep(5);
+ return 0; // Success
+ } else {
+ printf("Exiting...\n");
+ return 1; // Failure/Abort
+ }
+ }
